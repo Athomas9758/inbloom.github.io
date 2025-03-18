019bfb46624fba9905db55167b63f9c9f24509e3
gbrent7203 <gbrent7203@conestogac.on.ca>
2025-03-18 17:14:02 -0400

    commit

--- contactform.php
+++ contactform.php
@@ -4,6 +4,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST['email'];
     $message = $_POST['message'];
 
-    echo "Thank you for your message! We will get back to you soon!";
-}
+    $to = "your-email@example.com";
+    $subject = "New Contact Form Submission";
+    $headers = "From: $email";
+
+    $body = "Name: $name\nEmail: $email\n\nMessage:\n$message";
+
+    // Send email
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Thank you, $name! Your message has been sent.";
+    } else {
+        echo "Sorry, there was an error. Please try again later.";
+    }
 ?>
